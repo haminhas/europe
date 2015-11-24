8bfda717766d1111fafb1eb765f5978405c4f10b
Mohammad Humaid <mohammad.humaid@kcl.ac.uk>
2015-11-24 23:29:17 +0000

    Google maps fully functional with a bit of the information layout

--- app/src/main/java/boysenberry/europe/MapsActivity.java
+++ app/src/main/java/boysenberry/europe/MapsActivity.java
@@ -1,76 +1,113 @@
 package boysenberry.europe;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     private TextView mTextView;
+    private Geocoder geocoder;
+    private Marker marker;
+    private RelativeLayout rlInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+        mTextView = (TextView)findViewById(R.id.mTextView);
+        rlInfo = (RelativeLayout)findViewById(R.id.rlInfo);
+        rlInfo.setVisibility(View.INVISIBLE);
+        //rlInfo.setAlpha(0.95f);
+    }
 
     // Map API
     // https://developers.google.com/android/reference/com/google/android/gms/maps/package-summary
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        LatLng germany = new LatLng(52, 13);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(germany));
 
         mMap.setOnMapClickListener(this);
         mMap.setOnMapLongClickListener(this);
 
-        LatLng germany = new LatLng(52, 13);
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(germany));
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-
-            }
-        });
-
-
-        //mMap.addMarker(new MarkerOptions().position(germany).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(germany).title("Tap and hold to view infographics for European countries."));
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
-        mTextView.setText("tapped, point = " + latLng);
-        System.out.println("tapped, point = " + latLng);
+        double latitude = latLng.latitude;
+        double longitude = latLng.longitude;
+        mTextView.setText("tapped, point = " + latitude + " lng" + longitude);
+
     }
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-        mTextView.setText("tapped, point = " + latLng);
+
+        List<Address> addresses = new ArrayList<>();
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude,1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        android.location.Address address = addresses.get(0);
+
+        String countryName = "";
+        if (address != null) {
+            for (int i = 0; i < address.getMaxAddressLineIndex(); i++){
+                countryName = address.getCountryName();
+            }
+        }
+
+        // Removes previously placed marker.
+        if (marker != null) {
+            marker.remove();
+        }
+
+        //place marker where user just clicked
+        marker = mMap.addMarker(new MarkerOptions().position(latLng).title(countryName).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+
+        // changes the text view on top of the map
+        mTextView.setText("tapped!, point = " + latLng + "Country " + countryName);
+        rlInfo.setVisibility(View.VISIBLE);
+
+    }
+
+    public void closeInfo(View view) {
+        rlInfo.setVisibility(View.INVISIBLE);
+        marker.remove();
     }
 }
+
